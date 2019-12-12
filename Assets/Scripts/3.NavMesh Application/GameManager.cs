b75a4278491a2e4d5b3fdf1305829089ee8e2d7d
Dlee4428 <44447609+Dlee4428@users.noreply.github.com>
2019-12-11 21:36:07 -0500

    Initial Commit

---
+++ Assets/Scripts/3.NavMesh Application/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameManager : MonoBehaviour {
+    public GameObject[] spawningPoints;
+    public GameObject alien;
+    public GameObject player;
+
+    // Use this for initialization
+    IEnumerator Start () {
+		while (true)
+        {
+            // To do : Select one of the spawning points(spawningPoints) randomly
+            // and then instantiate an alien object
+            // Don't forget to set the alien's target to player
+            int spawn = Random.Range(0, spawningPoints.Length);
+
+            GameObject.Instantiate(alien, spawningPoints[spawn].transform.position, Quaternion.identity);
+           
+            if (player != null)
+            {
+                alien.GetComponent<Alien>().target = player;
+            }
+
+            yield return new WaitForSeconds(5f);
+        }
+	}
+}
---
+++ Assets/Scripts/3.NavMesh Application/ThirdPersonCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform target;
+    public float mouseSensitivity = 10;
+    public float disFromTarget = 5;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public bool lockCursor;
+    public float rotSmoothTime = 0.12f;
+    Vector3 rotSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw, pitch;
+
+    private void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(-pitch, yaw), ref rotSmoothVelocity, rotSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * disFromTarget;
+    }
+}
